f84be30dadccb09e6d43b36ec30e23b099156cf6
Prandan127 <140757974+Prandan127@users.noreply.github.com>
2025-03-24 00:24:35 +0300

    added minimal logic for the store

---
+++ coolguy1/Assets/Scripts/Inventory&Shop/Shop/ShopSlot.cs
@@ -0,0 +1,23 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ShopSlot : MonoBehaviour
+{
+    public ItemSO itemSO;
+    public TMP_Text itemNameText;
+    public TMP_Text priceText;
+    public Image itemImage;
+
+    private int price;
+
+    public void Initialize(ItemSO newItemSO, int price)
+    {
+        itemSO = newItemSO;
+        itemImage.sprite = itemSO.icon;
+        itemNameText.text = itemSO.name;
+        this.price = price;
+        priceText.text = price.ToString(); 
+    }
+}

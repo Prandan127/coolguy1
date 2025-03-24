a56a9c5b079ab8fa01fdbf10b0cbdd6f8bd55d4a
Prandan127 <140757974+Prandan127@users.noreply.github.com>
2025-03-24 22:40:49 +0300

    added info panel in the shop

--- coolguy1/Assets/Scripts/Inventory&Shop/Shop/ShopSlot.cs
+++ coolguy1/Assets/Scripts/Inventory&Shop/Shop/ShopSlot.cs
@@ -1,9 +1,10 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 
-public class ShopSlot : MonoBehaviour
+public class ShopSlot : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerMoveHandler
 {
     public ItemSO itemSO;
     public TMP_Text itemNameText;
@@ -11,13 +12,14 @@ public class ShopSlot : MonoBehaviour
     public Image itemImage;
 
     [SerializeField] private ShopManager shopManager;
+    [SerializeField] private ShopInfo shopInfo;
     public int price;
 
     public void Initialize(ItemSO newItemSO, int price)
     {
         itemSO = newItemSO;
         itemImage.sprite = itemSO.icon;
-        itemNameText.text = itemSO.name;
+        itemNameText.text = itemSO.itemName;
         this.price = price;
         priceText.text = price.ToString(); 
     }
@@ -25,5 +27,20 @@ public class ShopSlot : MonoBehaviour
     public void OnBuyButtonOnClicked()
     {
         shopManager.TryBuyItem(itemSO, price);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (itemSO != null) shopInfo.ShowItemInfo(itemSO);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        shopInfo.HideItemInfo();
+    }
+
+    public void OnPointerMove(PointerEventData eventData)
+    {
+        if (itemSO != null) shopInfo.FollowMouse();
     }
 }

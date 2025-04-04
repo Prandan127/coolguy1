939de60e93ee89257216d27ba7402df10de787fb
Prandan127 <140757974+Prandan127@users.noreply.github.com>
2025-04-04 08:16:13 +0300

    added labs

---
+++ coolguy1/Assets/Scripts/SceneChange/SceneChanger.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public string sceneToLoad;
+    public Animator fadeAnim;
+    public float fadeTime = 1;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            fadeAnim.Play("FadeToBlack0");
+            StartCoroutine(DelayFade());
+        }    
+    }
+
+    IEnumerator DelayFade()
+    {
+        yield return new WaitForSeconds(fadeTime);
+        SceneManager.LoadScene(sceneToLoad);
+    }
+}

c3f93fe89a120e73fce10c3453ae849d40ff2d81
Prandan127 <140757974+Prandan127@users.noreply.github.com>
2025-06-02 20:13:06 +0300

    added animator stub for tests

---
+++ coolguy1/Assets/Tests/Editor/AnimatorStub.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class AnimatorStub : MonoBehaviour
+{
+    public RuntimeAnimatorController runtimeAnimatorController;
+
+    public void Play(string stateName) { }
+
+    public void SetBool(string name, bool value) { }
+
+    public bool GetBool(string name) { return false; }
+}

ad30fde0dbceb380bf5d85a2d57e51d29e9641fd
Prandan127 <140757974+Prandan127@users.noreply.github.com>
2025-06-05 09:36:17 +0300

    added top users UI-test

--- coolguy1/Assets/Tests/Editor/AnimatorStub.cs
+++ coolguy1/Assets/Tests/Editor/AnimatorStub.cs
@@ -5,8 +5,6 @@ public class AnimatorStub : MonoBehaviour
     public RuntimeAnimatorController runtimeAnimatorController;
 
     public void Play(string stateName) { }
-
     public void SetBool(string name, bool value) { }
-
     public bool GetBool(string name) { return false; }
 }
